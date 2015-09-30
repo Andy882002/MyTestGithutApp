2a1b6a0362e7ae8716dcee9217be5526d92f7565
Anda <andylu882002@yahoo.com>
2015-09-29 17:19:13 -0700

    This  is testing GitHut

---
+++ SP6/MYJavaGithutApp/src/com/test/MYMaintest.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class MYMaintest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Testing Java GitHut");
+	}
+
+}
